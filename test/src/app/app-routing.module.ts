8052ded021bdd5eea9e804f5ae41bf4acd5bb0e5
Simon <jolet.simon@yahoo.com>
2020-02-04 11:04:28 +0100

    try go to go on

--- test/src/app/app-routing.module.ts
+++ test/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'ar',
+    loadChildren: () => import('./ar/ar.module').then( m => m.ArPageModule)
+  },
 ];
 
 @NgModule({
---
+++ test/src/app/ar/ar.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-ar',
+  templateUrl: './ar.page.html',
+  styleUrls: ['./ar.page.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ArPage implements OnInit {
+
+  constructor(private modalCtrl: ModalController) { }
+
+  ngOnInit() {
+    console.log("init");
+    
+
+  }
+
+}
